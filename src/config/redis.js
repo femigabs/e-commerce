83227955a7d3e0e77309294bb19d8bed92f3e26f
Oluwafemi  Babayemi <femi.gabs@gmail.com>
2020-08-21 11:58:23 +0100

    travisci added

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,9 +1,9 @@
 import redis from 'redis';
 import logger from './logger';
 
-const REDIS_URL = process.env.REDIS_URL || 6379;
+//const REDIS_URL = process.env.REDIS_URL || 6379;
 
-const client = redis.createClient({url: REDIS_URL});
+const client = redis.createClient({url: process.env.REDIS_URL});
 
 client.on('error', (error) => {
     logger.error(error);
