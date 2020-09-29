505ff85502e0b0903d35b7ff0fd092de31532798
Oluwafemi  Babayemi <femi.gabs@gmail.com>
2020-09-29 11:32:19 +0100

    login middleware updated

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,7 @@ const router = new Router();
 router.post("/register", UserMiddleware.signupMiddleWare, UserMiddleware.signupUser, UserController.signup);
 router.post("/confirmation", UserMiddleware.checkId, UserMiddleware.checkCode, UserMiddleware.checkExpiry, UserController.updateVerificationStatus);
 router.put("/confirmation", UserMiddleware.checkId, UserController.resendVerificationCode);
-router.post("/login", UserMiddleware.loginMiddleWare, UserMiddleware.checkVerification, UserMiddleware.loginUser, UserController.login);
+router.post("/login", UserMiddleware.loginMiddleWare, UserMiddleware.loginUser, UserMiddleware.checkVerification, UserController.login);
 router.post("/forgot-password", UserMiddleware.emailMiddleWare, UserController.forgetPassword)
 router.put("/reset-password", UserMiddleware.checkResetCode, UserMiddleware.checkExpiry, UserController.resetPassword)
 
